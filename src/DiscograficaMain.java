d1aa1546e5e157b9fa71a090389f59470fed4345
gand <zaken85@gmail.com>
2017-01-31 14:40:20 +0100

    Git

--- src/DiscograficaMain.java
+++ src/DiscograficaMain.java
@@ -1,8 +1,10 @@
+import objetos.Album;
+
 /**
  * Created by gand on 31/01/17.
  */
 public class DiscograficaMain {
     public static void main(String[] args) {
-
+        System.out.println();
     }
 }
